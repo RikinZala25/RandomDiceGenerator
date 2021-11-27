67641ebabfb9f6059bb032815284f24f7cc7097f
Rikin Zala <76729304+RikinZala25@users.noreply.github.com>
2021-11-27 16:39:59 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,10 +7,11 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomImageSourse2 = "images/dice" + randomNumber2 + ".png";
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSourse2);
 
+
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins";
-} else if (randomNumber1 < randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins";
+  document.querySelector("h1").innerHTML = "Play 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
 } else {
-  document.querySelector("h1").innerHTML = "Draw";
+  document.querySelector("h1").innerHTML = "Draw!";
 }
